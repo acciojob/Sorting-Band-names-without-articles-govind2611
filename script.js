9deb1c00db287d19df4609baa1cb43bb68905bb3
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-16 22:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-// Input array of band names
-let bandNames = ["The Beatles", "Rolling Stones", "Led Zeppelin", "Pink Floyd", "The Who", "The Doors"];
+// List of band names
+const bandNames = ["The Beatles", "Led Zeppelin", "Pink Floyd", "The Rolling Stones", "Aerosmith", "Anthrax", "Metallica"];
 
-// Function to remove articles from band names
-function removeArticle(bandName) {
-  // List of articles to remove
-  let articles = ["a", "an", "the"];
-  // Split band name into words
-  let words = bandName.split(" ");
-  // Check if first word is an article
-  if (articles.includes(words[0].toLowerCase())) {
-    // Remove the article
-    words.shift();
-  }
-  // Join the remaining words and return the modified band name
-  return words.join(" ");
+// Function to remove articles from the band name
+function removeArticles(bandName) {
+  const articles = ["a", "an", "the"];
+  const words = bandName.split(" ");
+  return words.filter((word) => !articles.includes(word.toLowerCase())).join(" ");
 }
 
-// Sort the band names in lexicographic order excluding articles
-bandNames.sort(function(a, b) {
-  return removeArticle(a).localeCompare(removeArticle(b));
+// Sort the band names without articles in lexicographic order
+const sortedBandNames = bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Generate the HTML code
+let htmlCode = "<ul id='band'>\n";
+sortedBandNames.forEach((bandName) => {
+  htmlCode += `<li>${removeArticles(bandName)}</li>\n`;
 });
+htmlCode += "</ul>";
 
-// Get the ul element with id 'band'
-let ulElement = document.getElementById("band");
-
-// Loop through the sorted band names and add them as li elements to the ul element
-for (let i = 0; i < bandNames.length; i++) {
-  let liElement = document.createElement("li");
-  liElement.textContent = bandNames[i];
-  ulElement.appendChild(liElement);
-}
+// Print the HTML code
+console.log(htmlCode);
