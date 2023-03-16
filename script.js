6b205215839dae03f8d004bc531b2af0d9947020
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-03-16 22:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,22 @@
-let bandNames = ["The Beatles", "Rolling Stones", "Led Zeppelin", "Pink Floyd", "The Who", "The Doors"];
+// List of band names
+const bandNames = ["The Beatles", "Led Zeppelin", "Pink Floyd", "The Rolling Stones", "Aerosmith", "Anthrax", "Metallica"];
 
-const express = require('express');
-const path = require('path');
-// Function to remove articles from band names
-function removeArticle(bandName) {
-  // List of articles to remove
-  let articles = ["a", "an", "the"];
-  // Split band name into words
-  let words = bandName.split(" ");
-  // Check if first word is an article
-  if (articles.includes(words[0].toLowerCase())) {
-    // Remove the article
-    words.shift();
-  }
-  // Join the remaining words and return the modified band name
-  return words.join(" ");
+// Function to remove articles from the band name
+function removeArticles(bandName) {
+  const articles = ["a", "an", "the"];
+  const words = bandName.split(" ");
+  return words.filter((word) => !articles.includes(word.toLowerCase())).join(" ");
 }
 
-const app = express();
-// Sort the band names in lexicographic order excluding articles
-bandNames.sort(function(a, b) {
-  return removeArticle(a).localeCompare(removeArticle(b));
-});
+// Sort the band names without articles in lexicographic order
+const sortedBandNames = bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
 
-app.use(express.static(__dirname))
-// Get the ul element with id 'band'
-let ulElement = document.getElementById("band");
+// Generate the HTML code
+let htmlCode = "<ul id='band'>\n";
+sortedBandNames.forEach((bandName) => {
+  htmlCode += `<li>${removeArticles(bandName)}</li>\n`;
+});
+htmlCode += "</ul>";
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/main.html'));
-});
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
-module.exports = app;
-// Loop through the sorted band names and add them as li elements to the ul element
-for (let i = 0; i < bandNames.length; i++) {
-  let liElement = document.createElement("li");
-  liElement.textContent = bandNames[i];
-  ulElement.appendChild(liElement);
-}
+// Print the HTML code
+console.log(htmlCode);
